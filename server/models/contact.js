056c72c1f94fcb5d5a52c64d22aea23a7c73256e
Alex Tse <ax@Alexs-MacBook-Pro.local>
2017-09-04 01:10:03 +0800

    update server.js

--- server/models/contact.js
+++ server/models/contact.js
@@ -1,45 +1,85 @@
+// eng_name: string,
+//   post: string,
+//     phone_no: string,
+//       team_no: string,
+//         section_no: string,
+//           firstname: string,
+//             surname: string,
+//               last_update_date: string
+
 var mongoose = require('mongoose');
 
 var Contact = mongoose.model('Contact', {
-    firstname:{
-      type: String
-      // ,
-      // required: true,
-      // minlength: 1
-    },
-    surname: {
-      type: String
-      // ,
-      // required: true,
-      // minlength: 1
-    },
-    postTitle: {
-      type: String
-      // ,
-      // required: true,
-      // minlength: 1
-    },
-    extNo: {
-      type: String
-      // ,
-      // required: true,
-      // minlength: 1
-    },
-    teamNo: {
-      type: String
-      // ,
-      // required: true,
-      // minlength: 1
-    },
-    sectionNo: {
-      type: String
-      // ,
-      // required: true,
-      // minlength: 1
-    },
-    status: {
-      type: String
-    }
+  eng_name: {
+    type: String
+  },
+  post: {
+    type: String
+  },
+  phone_no: {
+    type: String
+  },
+  team_no: {
+    type: String
+  },
+  section_no: {
+    type: String
+  },
+  firstname: {
+    type: String
+  },
+  surname: {
+    type: String
+  },
+  last_update_date: {
+    type: String
+  }
 });
 
-module.exports = {Contact};
+module.exports = { Contact };
+
+// var mongoose = require('mongoose');
+
+// var Contact = mongoose.model('Contact', {
+//     firstname:{
+//       type: String
+//       // ,
+//       // required: true,
+//       // minlength: 1
+//     },
+//     surname: {
+//       type: String
+//       // ,
+//       // required: true,
+//       // minlength: 1
+//     },
+//     postTitle: {
+//       type: String
+//       // ,
+//       // required: true,
+//       // minlength: 1
+//     },
+//     extNo: {
+//       type: String
+//       // ,
+//       // required: true,
+//       // minlength: 1
+//     },
+//     teamNo: {
+//       type: String
+//       // ,
+//       // required: true,
+//       // minlength: 1
+//     },
+//     sectionNo: {
+//       type: String
+//       // ,
+//       // required: true,
+//       // minlength: 1
+//     },
+//     status: {
+//       type: String
+//     }
+// });
+
+// module.exports = {Contact};
--- server/server.js
+++ server/server.js
@@ -12,25 +12,134 @@ app.use(bodyParser.json());
 
 app.post('/contacts',(req, res) => {
   // console.log(req.body);
-  var contact = new Contact({
-
-    // firstname: req.body.firstname,
-    // surname: req.body.surname,
-    // postTitle: req.body.postTitle,
-    // extNo: req.body.extNo,
-    // teamNo: req.body.teamNo,
-    // sectionNo: req.body.sectionNo,
-    // status: req.body.status
-    firstname: 'Alex',
-    surname: 'Tse',
-    postTitle: 'SAT/16',
-    extNo: '7289',
-    teamNo: '23',
-    sectionNo: '2',
-    status: 'Current'
-  });
+  var contact = new Contact(
+    {
+      eng_name: 'Jimmy Lam',
+      post: 'SITM/D',
+      phone_no: '2761-5886',
+      team_no: 'N/A',
+      section_no: '2',
+      firstname: 'Cheuk Ming',
+      surname: 'Lam',
+      last_update_date: '2017-09-01'
+    }
+  );
 
   contact.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+
+  var contact2 = new Contact(
+    {
+      eng_name: 'Edna Tam',
+      post: 'ITM/23',
+      phone_no: '2761-6315',
+      team_no: '23',
+      section_no: '2',
+      firstname: 'Yuen Har',
+      surname: 'Tam',
+      last_update_date: '2017-09-01'
+    }
+  );
+
+  contact2.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+
+  var contact3 = new Contact(
+    {
+      eng_name: 'John Cheung',
+      post: 'SAC/30',
+      phone_no: '2761-6579',
+      team_no: '23',
+      section_no: '2',
+      firstname: 'Kim Hung',
+      surname: 'Cheung',
+      last_update_date: '2017-09-01'
+    }
+  );
+
+  contact3.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+
+  var contact4 = new Contact(
+    {
+      eng_name: 'Alice Li',
+      post: 'SAC/35',
+      phone_no: '2761-7834',
+      team_no: '23',
+      section_no: '2',
+      firstname: 'Yat Hung',
+      surname: 'Li',
+      last_update_date: '2017-09-01'
+    }
+  );
+
+  contact4.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+
+  var contact5 = new Contact(
+    {
+      eng_name: 'Alex Tse',
+      post: 'SAT/16',
+      phone_no: '2761-7289',
+      team_no: '23',
+      section_no: '2',
+      firstname: 'Chun Hin',
+      surname: 'Tse',
+      last_update_date: '2017-09-01'
+    }
+  );
+
+  contact5.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+
+  var contact6 = new Contact(
+    {
+      eng_name: 'Mak Wong',
+      post: 'PC/43',
+      phone_no: '2761-7456',
+      team_no: '23',
+      section_no: '2',
+      firstname: 'Kam Fat',
+      surname: 'Wong',
+      last_update_date: '2017-09-01'
+    }
+  );
+
+  contact6.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+
+  var contact7 = new Contact(
+    {
+      eng_name: 'Yozki Yiu',
+      post: 'PC/46',
+      phone_no: '2761-7509',
+      team_no: '23',
+      section_no: '2',
+      firstname: 'Chi Fu',
+      surname: 'Yiu',
+      last_update_date: '2017-09-01'
+    }
+  );
+
+  contact7.save().then((doc) => {
     res.send(doc);
   }, (e) => {
     res.status(400).send(e);
@@ -47,14 +156,14 @@ app.get('/contacts', (req, res) => {
 
 //GET /todos/1234123
   //404 - send back empty send
-app.get('/contacts/:id', (req, res) => {
-  // res.send(req.params);
-  var id = req.params.id;
+// app.get('/contacts/:id', (req, res) => {
+//   // res.send(req.params);
+//   var id = req.params.id;
 
-  //valid id using isValid
-  if (!ObjectID.isValid(id)) {
-    return res.status(404).send();
-  }
+//   //valid id using isValid
+//   if (!ObjectID.isValid(id)) {
+//     return res.status(404).send();
+//   }
 
 
   //findById
@@ -63,15 +172,15 @@ app.get('/contacts/:id', (req, res) => {
     // no todo - send back 404 with empty body
   //error
     //400 - and send empty body back
-  Todo.findById(id).then((contact) => {
-    if (!contact) {
-      return res.status(404).send();
-    }
-    res.send({contact});
-  }).catch((e) => {
-    res.status(400).send();
-  });
-});
+//   Todo.findById(id).then((contact) => {
+//     if (!contact) {
+//       return res.status(404).send();
+//     }
+//     res.send({contact});
+//   }).catch((e) => {
+//     res.status(400).send();
+//   });
+// });
 
 //
 app.listen(port, () => {
